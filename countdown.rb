6d31d3134d59e6d13243749938ba4f9adace41a7
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-04 17:36:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,17 @@
-def countdown(x)
-  x = 10
-   while x > 0
-    puts "#{x} SECOND(S)!"
-    x -= 1
+def countdown(num)
+  num = 10
+   while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
 end	
  return "HAPPY NEW YEAR!"
 end
 
- def countdown_with_sleep(x)
-x = 10
-while x > 0
+ def countdown_with_sleep(num)
+num = 10
+while num > 0
   sleep 1
-  puts "#{x} SECOND(S)!"
-    x -= 1
+  puts "#{num} SECOND(S)!"
+    num -= 1
   end
 end
